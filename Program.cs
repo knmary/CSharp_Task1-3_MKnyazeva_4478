4ab7dbc146a5fea4a4a80b4272e10431020c6792
Maria Knyazeva <m.knyazeva@mail.ru>
2023-06-17 00:35:16 +0400

    Program.cs and .gitignore for Task1-3 added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+Console.WriteLine("Введите число");
+string inputstring = Console.ReadLine();
+int number = Convert.ToInt32(inputstring);
+if ((number % 2) == 0)
+{
+  Console.WriteLine($"{number} явлется чётным числом");
+}
+else
+{
+   Console.WriteLine($"{number} НЕ явлется чётным числом"); 
+}
